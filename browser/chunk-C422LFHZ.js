e3d6092ccae3dcb1abb3d7255a6cd642b8f7595f
Walace Maia <walace_srm@hotmail.com>
2025-05-06 18:52:56 -0300

    Auto-generated commit

---
+++ browser/chunk-C422LFHZ.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-LZJBAANE.js";import"./chunk-CESVDTFF.js";import"./chunk-NH7O73XC.js";import"./chunk-4RGYXDAY.js";import"./chunk-C5RQ2IC2.js";import"./chunk-SV7S5NYR.js";import"./chunk-RW4GY4BD.js";export{b as iosTransitionAnimation,a as shadow};

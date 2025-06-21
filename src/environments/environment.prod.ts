4fdb6f0f7ea3ee1c3752ce6bb9d4c8bd01c5a73f
Walace Rodrigues <walace_srm@hotmail.com>
2025-06-20 21:09:25 -0300

    ajuestes gerais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBAnCRy0asqvkxOkpouI1lbqKXeLjSt9yI",
+    authDomain: "fast-food-38e89.firebaseapp.com",
+    databaseURL: "https://fast-food-38e89-default-rtdb.firebaseio.com",
+    projectId: "fast-food-38e89",
+    storageBucket: "fast-food-38e89.firebasestorage.app",
+    messagingSenderId: "587247956537",
+    appId: "1:587247956537:web:ce9599bb7a79dfd0f153e8"
+  }
 };
